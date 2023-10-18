96624b82f0de8c2643456eae8b541779e27e58be
SANDESH LAVHANDE <56137576+sandylavhande@users.noreply.github.com>
2023-10-19 02:07:58 +0530

    Add files via upload

---
+++ CWscript.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -x
+touch log_file
+log_file="/ec2-user/script.sh/log_file"
+
+exec &>> "$log_file"
+
+servstat=$(service httpd status)
+Servicename="Apache"
+AWS_CLI_CMD="aws cloudwatch put-metric-data --region us-east-1"
+
+if [[ $servstat == *"active (running)"* ]]; then
+ 
+  $AWS_CLI_CMD --namespace "ApacheServerStatus" --metric-name "ServerStatus" --value 1 --dimensions "Service=$Servicename"
+else
+
+  $AWS_CLI_CMD --namespace "ApacheServerStatus" --metric-name "ServerStatus" --value 0 --dimensions "Service=$Servicename"
+fi
+
+set +x
+
+
+#install Apache server
+# install crontab
+# install 
+#install CWAgent
+#create iam usser for cw full access and with credentials for aws configure
+#AKIASZCUV3TCK2C2ZQRO
+#CEzGfMMFUXEtjURVy0FQFrN2Mjfu35rq6khSEYBe
+# */1 * * * * /bin/bash /home/ec2-user/script.sh
